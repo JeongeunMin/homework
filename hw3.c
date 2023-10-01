fa0b1dc520fffe6a15f869c9e17a1e657e5b65b8
JeongeunMin <unboy77@gmail.com>
2023-10-02 01:04:23 +0900

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
-#define  A_MIN 184*0.85
-#define  B_MIN 184*0.75
-#define  C_MIN 184*0.65
-#define  D_MIN 184*0.55
-
 #define MAX_POINTS 184
 #define MAX_Q_POINT 84
 
-typedef struct {
-    double ept;
-    double epc;
-} gradePoint;
+double calPoint(char dgr, double qpt) {
 
-gradePoint calPoint(char dgr, double qpt) {
-
-    gradePoint gp;
     double dg;
     double totalPoints;
     double examPoints;
@@ -26,31 +15,28 @@ gradePoint calPoint(char dgr, double qpt) {
 
     examPoints = MAX_POINTS*grade_arr[dgr%'A'] - qpt;
     examPercents = examPoints/MAX_POINTS;
-    
-    gp.ept = examPoints;
-    gp.epc = examPercents*100;
 
-    return gp;
+    return examPoints;
 }
 
 int main() {
 
 int qpt; //earned quiz point
 char dgr; //desired grade
-gradePoint gp;
+double ep;
 
 printf("Enter desired letter grade -> ");
 scanf("%c", &dgr);
 printf("Enter points earned to date (max 84) -> ");
 scanf("%d", &qpt);
-gp = calPoint(dgr, qpt);
+ep = calPoint(dgr, qpt);
 printf("Points earned to date: %.2lf (%.2lf) \n", (double)qpt, ((double)qpt/(double)MAX_Q_POINT)*100); // "%" 출력 해줘야함
 printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
-printf("Points required to earn %c grade: %.2lf (%.2lf)\n", dgr, gp.ept, gp.epc);// "%" 출력 해줘야함
-printf("Minimum exam questions correct: %d (%.2lf)\n",(int)ceil(gp.ept/3), (ceil(gp.ept/3)/35)*100);// "%" 출력 해줘야함
-gp = calPoint((int)(dgr+round((double)(dgr-65)/(double)5)+1), qpt);
-printf("Points required to earn %c grade: %.2lf (%.2lf)\n",(char)(dgr+round((double)(dgr-65)/(double)5)+1), gp.ept, gp.epc);// "%" 출력 해줘야함
-printf("Minimum exam questions correct: %d (%.2lf)\n",(int)ceil(gp.ept/3), (ceil(gp.ept/3)/35)*100);// "%" 출력 해줘야함
+printf("Points required to earn %c grade: %.2lf (%.2lf)\n", dgr, ep, (ep/MAX_POINTS)*100);// "%" 출력 해줘야함
+printf("Minimum exam questions correct: %d (%.2lf)\n",(int)ceil(ep/3), (ceil(ep/3)/35)*100);// "%" 출력 해줘야함
+ep = calPoint((int)(dgr+round((double)(dgr-65)/(double)5)+1), qpt);
+printf("Points required to earn %c grade: %.2lf (%.2lf)\n",(char)(dgr+round((double)(dgr-65)/(double)5)+1), ep, (ep/MAX_POINTS)*100);// "%" 출력 해줘야함
+printf("Minimum exam questions correct: %d (%.2lf)\n",(int)ceil(ep/3), (ceil(ep/3)/35)*100);// "%" 출력 해줘야함
 printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
 //printf("Points Earned : %d , Grade Earned: %c \n", totalPoints, courseGrade);
 return 0;
